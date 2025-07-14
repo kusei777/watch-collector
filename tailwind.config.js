d27f3ad65a55f6545016cade37a52f3da9c6f30c
khian <pangilinanmarkkhian@gmail.com>
2025-07-15 04:25:31 +0800

    git commit -m "Added my component"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+tailwind.config.js
+   js
+   module.exports = {
+     content: [
+       "./src/**/*.{js,jsx,ts,tsx}",
+     ],
+     theme: {
+       extend: {},
+     },
+     plugins: [],
+   }
+   
